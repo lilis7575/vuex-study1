20be32e9032852883138923b35b47bcc1af797d1
leejh <leejh@biztechpartners.co.kr>
2019-04-29 11:23:15 +0900

    MUTATIONS | VueJS 3 & Vuex | 기초배우기

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <v-container>
     <h1>Home</h1>
-    <h4>모든 유저의 수 : {{ $store.getters.allUsersCount }}</h4>
+    <h4>모든 유저의 수 : {{ allUsersCount }}</h4>
   </v-container>
 </template>
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["allUsersCount"])
+  }
+};
+</script>
